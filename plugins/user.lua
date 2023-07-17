59964a5ae73fb7515a6105bbf6fb0aabb85c90ba
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-07-17 15:22:58 +0200

    Replace leap with flash

--- plugins/user.lua
+++ plugins/user.lua
@@ -73,16 +73,32 @@ return {
     config = function() require("todo-comments").setup() end,
   },
   {
-    "ggandor/leap.nvim",
-    dependencies = {
-      "tpope/vim-repeat",
-    },
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
     keys = {
-      "s",
-      "S",
-      "gs",
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      {
+        "S",
+        mode = { "n", "o" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search",
+      },
     },
-    config = function() require("leap").add_default_mappings() end,
   },
   {
     "github/copilot.vim",
