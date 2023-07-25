e6b3c6b3f3974345fb9ddd55b5767fb018eb9e15
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-07-25 16:41:51 +0200

    Fix lsp_signature

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -133,7 +133,7 @@ return {
   },
   {
     "ray-x/lsp_signature.nvim",
-    event = "LspAttach",
+    event = "VeryLazy",
     config = function()
       require("lsp_signature").setup {
         hint_enable = false,
